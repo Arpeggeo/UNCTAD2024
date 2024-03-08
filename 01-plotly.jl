66d8a840eeb5b3e45856bec8f270ceeeb42c546e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-03-08 15:22:17 -0300

    Initial commit

---
+++ 01-plotly.jl
@@ -0,0 +1,27 @@
+using CSV
+using PlotlyJS
+using Statistics
+
+## load relevant data
+
+address = CSV.File("data/clean/address.csv")
+
+## global map settings
+
+lon = address.LONGITUDE
+lat = address.LATITUDE
+
+layout = Layout(
+  mapbox_style = "open-street-map",
+  mapbox_center_lon = median(lon),
+  mapbox_center_lat = median(lat),
+  mapbox_zoom = 10
+)
+
+## density map of addresses
+
+dens = densitymapbox(lon = lon, lat = lat, radius = 4)
+
+## display visualization
+
+plot(dens, layout)
