e7719f9b13f1d8e8a611fc409740789c8420a44a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-04-04 09:09:57 -0300

    Add answers

--- 01-plotly.jl
+++ 01-plotly.jl
@@ -21,6 +21,33 @@ dict = Dict(
 
 ## density map of coordinates
 
+lon = data.LONGITUDE
+lat = data.LATITUDE
+key = [dict[k] for k in data.COD_ESPECIE]
+
+trace = densitymapbox(lat = lat, lon = lon, customdata = key,
+                      radius = 4, colorscale = "YlOrRd",
+                      hovertemplate = """
+                      <b>%{customdata}</b><br>
+                      latitude: %{lat}<br>
+                      longitude: %{lon}
+                      <extra></extra>
+                      """)
+
 ## visualization settings
 
+layout = Layout(
+  height = "800",
+  #mapbox_style = "open-street-map",
+  #mapbox_style = "mapbox://styles/mapbox/satellite-streets-v12",
+  #mapbox_style = "mapbox://styles/mapbox/streets-v12",
+  mapbox_style = "mapbox://styles/mapbox/navigation-night-v1",
+  mapbox_accesstoken = ENV["MAPBOX_TOKEN"],
+  mapbox_center_lon = median(lon),
+  mapbox_center_lat = median(lat),
+  mapbox_zoom = 10
+)
+
 ## display figure
+
+fig = plot(trace, layout)
--- 02-dash.jl
+++ 02-dash.jl
@@ -2,4 +2,14 @@ using Dash
 
 ## setup dash app
 
+app = dash()
+
+app.layout = html_div() do
+    html_h1("UNCTAD 2024"),
+    html_div("Nova Friburgo density map"),
+    dcc_graph(id = "density-map", figure = fig)
+end
+
 ## run app on web browser
+
+run_server(app)
